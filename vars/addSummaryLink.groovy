f054408a7f6c56c6ef7909385e0c1cc7a9699886
Karthik Rajasekaran <karajas@microsoft.com>
2017-06-08 15:32:42 -0700

    Remove escaping

--- vars/addSummaryLink.groovy
+++ vars/addSummaryLink.groovy
@@ -5,12 +5,12 @@ def call (String header, Map urls, boolean isError = false) {
     def newSummary = manager.createSummary("terminal.gif")
     newSummary.appendText("<b>${header}</b><ul>", false)
     if (urls.size() > 0) {
-        urls.each { linkDescription, linkToAdd ->
+        for ( item in urls ) {
             if(!isError) {
-                newSummary.appendText("<li><a href=\\\"\${linkToAdd}\\\">\${linkDescription}</a></li>", false)
+                newSummary.appendText("<li><a href=\""+item.value+"\" \\>"+item.key+"</a></li>", false)
             }
             else {
-                newSummary.appendText("<li><a href=\\\"\${linkToAdd}\\\">\${linkDescription}</a></li>", false, false, false, "red")
+                newSummary.appendText("<li><a href=\""+item.value+"\" \\>"+item.key+"</a></li>", false, false, false, "red")
             }
             
         }
