f64e7707576b3b721feefaeae0bbdd60f28e4c90
Karthik Rajasekaran <karajas@microsoft.com>
2017-06-08 13:07:07 -0700

    Fix brackets

--- vars/addSummaryLink.groovy
+++ vars/addSummaryLink.groovy
@@ -1,12 +1,18 @@
 // Given a header and text, add them to the status page
 // as a list
 
-def call (String header, Map urls) {
+def call (String header, Map urls, boolean isError = false) {
     def newSummary = manager.createSummary("terminal.gif")
     newSummary.appendText("<b>${header}</b><ul>", false)
-    if (${urls}.size() > 0) {
-        ${urls}.each { linkDescription, linkToAdd ->
-            newSummary.appendText("<li><a href=\\\"\${linkToAdd}\\\">\${linkDescription}</a></li>", false)
+    if (urls.size() > 0) {
+        urls.each { linkDescription, linkToAdd ->
+            if(!isError) {
+                newSummary.appendText("<li><a href=\\\"\${linkToAdd}\\\">\${linkDescription}</a></li>", false)
+            }
+            else {
+                newSummary.appendText("<li><span style="background-color: #FF0000"><a href=\\\"\${linkToAdd}\\\">\${linkDescription}</a></span></li>", false)
+            }
+            
         }
     }
     newSummary.appendText("</ul>", false)
