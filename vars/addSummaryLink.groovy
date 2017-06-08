184a2c0da59fa9f543540420fc15a43d8e010b9b
Karthik Rajasekaran <karajas@microsoft.com>
2017-06-08 11:49:48 -0700

    Fix string interpolation

--- vars/addSummaryLink.groovy
+++ vars/addSummaryLink.groovy
@@ -3,10 +3,10 @@
 
 def call (String header, Map urls) {
     def newSummary = manager.createSummary("terminal.gif")
-    newSummary.appendText("<b>$header</b><ul>", false)
+    newSummary.appendText("<b>${header}</b><ul>", false)
     if (${urls}.size() > 0) {
         ${urls}.each { linkDescription, linkToAdd ->
-            newSummary.appendText("<li><a href=\\\"\$linkToAdd\\\">\$linkDescription</a></li>", false)
+            newSummary.appendText("<li><a href=\\\"\${linkToAdd}\\\">\${linkDescription}</a></li>", false)
         }
     }
     newSummary.appendText("</ul>", false)
